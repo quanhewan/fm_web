3e17a6cda56a4970dd1fe027c03df38af5d2aff8
quanhewan <quanhewan@gmail.com>
2016-06-08 22:20:50 -0700

    added ajax contact form - ready for launch

--- js/main.js
+++ js/main.js
@@ -89,4 +89,43 @@ $(document).ready(function () {
         $(".novel-preview-mask-final").fadeOut();
     });
 
+    // ajax for contact form.
+    var form = $('#ajax-contact');
+    var formMessages = $('#form-messages');
+    $(form).submit(function(event) {
+        event.preventDefault();
+        var formData = $(form).serialize();
+        $.ajax({
+            type: 'POST',
+            url: $(form).attr('action'),
+            data: formData
+        }).done(function(response) {
+            // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
+
+            // Set the message text.
+            $(formMessages).text(response);
+
+            // Clear the form.
+            $('#name').val('');
+            $('#email').val('');
+            $('#message').val('');
+        }).fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('出错了！邮件没有发送。');
+            }
+        });
+
+
+    });
+
+
 });
